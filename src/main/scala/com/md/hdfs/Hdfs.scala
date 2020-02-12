afbba65588fa0c4e041d80a7e012df9369a8fa9d
mriduld <d.mridul@gmail.com>
2020-02-12 22:28:57 +1100

    First commit

--- src/main/scala/com/md/hdfs/Hdfs.scala
+++ src/main/scala/com/md/hdfs/Hdfs.scala
@@ -1,22 +1,22 @@
 package com.md.hdfs
 
 import org.apache.hadoop.conf.Configuration
-import org.apache.hadoop.fs.{FileStatus, FileSystem, Path}
+import org.apache.hadoop.fs.{FileSystem, Path}
 import zio.RIO
 
 object Hdfs {
-	def fileSystem(path: Path): Hdfs[FileSystem] = 
-		for {
-			conf <- RIO.environment[Configuration]
-			fs   <- RIO(FileSystem.get(path.toUri, conf))
-		} yield fs
+  def fileSystem(path: Path): Hdfs[FileSystem] =
+    for {
+      conf <- RIO.environment[Configuration]
+      fs <- RIO(FileSystem.get(path.toUri, conf))
+    } yield fs
 
-	def withFileSystem[A](path: Path)(f: FileSystem => A): Hdfs[A] =
-		for {
-	      fs <- fileSystem(path)
-	      a  <- RIO(f(fs))
-	    } yield a
+  def withFileSystem[A](path: Path)(f: FileSystem => A): Hdfs[A] =
+    for {
+      fs <- fileSystem(path)
+      a <- RIO(f(fs))
+    } yield a
 
-	def exists(path: Path): Hdfs[Boolean] =
-    	withFileSystem(path)(_.exists(path))    
+  def exists(path: Path): Hdfs[Boolean] =
+    withFileSystem(path)(_.exists(path))
 }
