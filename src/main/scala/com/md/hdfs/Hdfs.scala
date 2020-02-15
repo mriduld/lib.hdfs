d0c53c48cb84abe5ec0d62b7ab42d78a650d0e6f
mriduld <d.mridul@gmail.com>
2020-02-15 14:29:02 +1100

    Error Types

--- src/main/scala/com/md/hdfs/Hdfs.scala
+++ src/main/scala/com/md/hdfs/Hdfs.scala
@@ -1,22 +1,33 @@
 package com.md.hdfs
 
+import java.io.IOException
+
 import org.apache.hadoop.conf.Configuration
 import org.apache.hadoop.fs.{FileSystem, Path}
-import zio.RIO
+import zio.ZIO
 
 object Hdfs {
+  def safeIO[A](a: => A): Hdfs[A] =
+    ZIO(a).catchAll {
+      case e: IOException => ZIO.fail(HdfsIOError(e))
+      case e: Exception   => ZIO.fail(MessageHdfsError(e.getMessage))
+    }
+
   def fileSystem(path: Path): Hdfs[FileSystem] =
-    for {
-      conf <- RIO.environment[Configuration]
-      fs <- RIO(FileSystem.get(path.toUri, conf))
-    } yield fs
+    ZIO.accessM[Configuration]{ conf =>
+      safeIO(FileSystem.get(path.toUri, conf))
+    }
 
   def withFileSystem[A](path: Path)(f: FileSystem => A): Hdfs[A] =
     for {
       fs <- fileSystem(path)
-      a <- RIO(f(fs))
+      a  <- safeIO(f(fs))
     } yield a
 
   def exists(path: Path): Hdfs[Boolean] =
     withFileSystem(path)(_.exists(path))
 }
+
+sealed trait HdfsError
+case class HdfsIOError(error: IOException) extends HdfsError
+case class MessageHdfsError(message: String) extends HdfsError
--- src/main/scala/com/md/hdfs/package.scala
+++ src/main/scala/com/md/hdfs/package.scala
@@ -1,9 +1,9 @@
 package com.md
 
-import zio.RIO
-import java.io.IOException
 import org.apache.hadoop.conf.Configuration
+import zio.ZIO
 
 package object hdfs {
-	type Hdfs[A] = RIO[Configuration, A]
+	//type Hdfs[A] = RIO[Configuration, A]
+	type Hdfs[A] = ZIO[Configuration, HdfsError, A]
 }
